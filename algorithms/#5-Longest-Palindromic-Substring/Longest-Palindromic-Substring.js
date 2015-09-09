4c91d0380dba950d424796ede1c1a23723bae46e
Gonster <gs5647@gmail.com>
2015-09-09 15:38:17 +0800

    better approach

--- algorithms/#5-Longest-Palindromic-Substring/Longest-Palindromic-Substring.js
+++ algorithms/#5-Longest-Palindromic-Substring/Longest-Palindromic-Substring.js
@@ -8,23 +8,21 @@ var longestPalindrome = function(s) {
     var rl = 1;
     var res = s[0];
     var max = (l > 1000) ? 1000 : l;
-    for (var j = max; j > 0; j--) {
-        for (var i = 0; i < l - j + 1; i++) {
-            var ss = s.substr(i, j);
-            if (isPalindromic(ss)) return ss;
+
+    for (var i = 0; i < l; i += 0.5) {
+        var pair1 = Math.floor(i - 0.5);
+        var pair2 = Math.ceil(i + 0.5);
+
+        for (; pair1 > -1 && pair2 < l ; pair1--,pair2++) {
+            if(s[pair1] !== s[pair2]) {
+                break;
+            }
+        }
+        var currentLength = pair2 - pair1 - 1;
+        if(currentLength > rl) {
+            rl = currentLength;
+            res = s.substr(pair1 + 1, currentLength);
         }
     }
     return res;
 };
-
-function isPalindromic(s) {
-    var l = s.length;
-    if (l === 1) return true;
-    var mid = l / 2;
-    var s1 = s.substring(0, Math.floor(mid));
-    var s2 = s.substring(Math.ceil(mid));
-    for (var i = s1.length - 1, j = 0; i > -1; j++, i--) {
-        if (s1[i] !== s2[j]) return false;
-    }
-    return true;
-}
