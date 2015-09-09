8779d4892203f3fc1e2db48c5c459bd0eba323f9
Gonster <gs5647@gmail.com>
2015-09-09 13:16:39 +0800

    only perform better when p string is very long

--- algorithms/#5-Longest-Palindromic-Substring/Longest-Palindromic-Substring.js
+++ algorithms/#5-Longest-Palindromic-Substring/Longest-Palindromic-Substring.js
@@ -4,26 +4,27 @@
  */
 var longestPalindrome = function(s) {
     var l = s.length;
+    if(l < 1) return '';
     var rl = 1;
-    var res = '';
-    for(var i=0;i<l;i++){
-        var max = (l-i >1000)?1000:(l-i);
-        for(var j=max;j>-1;j--){
+    var res = s[0];
+    var max = (l > 1000) ? 1000 : l;
+    for (var j = max; j > 0; j--) {
+        for (var i = 0; i < l - j + 1; i++) {
             var ss = s.substr(i, j);
-            if(j>rl && isPalindromic(ss)){rl=j;res = ss;break;}
+            if (isPalindromic(ss)) return ss;
         }
     }
     return res;
 };
 
-function isPalindromic(s){
+function isPalindromic(s) {
     var l = s.length;
-    if(l === 1) return true;
-    var mid = l/2;
+    if (l === 1) return true;
+    var mid = l / 2;
     var s1 = s.substring(0, Math.floor(mid));
     var s2 = s.substring(Math.ceil(mid));
-    for(var i = s1.length-1, j = 0; i>-1; j++,i--) {
-        if(s1[i]!==s2[j]) return false;
+    for (var i = s1.length - 1, j = 0; i > -1; j++, i--) {
+        if (s1[i] !== s2[j]) return false;
     }
     return true;
 }
